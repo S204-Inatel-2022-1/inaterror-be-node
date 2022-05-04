b17c29d415dd9a5a1b511c3ac90dea4a8cd00ff9
bruno <bruno.3av@gmail.com>
2022-05-04 13:59:56 -0300

    Heroku

--- dist/src/server.js
+++ dist/src/server.js
@@ -7,7 +7,7 @@ const express_1 = __importDefault(require("express"));
 const router_1 = __importDefault(require("./controller/router"));
 const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
-const port = 8000;
+const port = process.env.PORT || 3000;
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use(router_1.default);
--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import routes from "./controller/router";
 import cors from "cors";
 
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(routes);
