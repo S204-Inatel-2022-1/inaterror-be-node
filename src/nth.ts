a7566aa037400cb97445a15dd19c7d712eedc8ba
bruno <bruno.3av@gmail.com>
2022-05-09 17:03:52 -0300

    replace routes

--- src/nth.ts
+++ src/nth.ts
@@ -1,7 +1,37 @@
-import SightingsDAO from "./controller/database/sightingDAO";
-import * as dotenv from 'dotenv';
-dotenv.config();
-const db = new SightingsDAO();
+import UserModel from "./model/UserModel";
+import { SightingType, UserType } from "./types";
+import { query } from "express";
 
+const main = async () => {
+  const model = new UserModel();
 
+  // const user: UserType  = {
+  //   name: "Robertaa",
+  //   pass: "monsa450PPaaa",
+  // };
 
+  // const sighting: SightingType = {
+  //   name: "Monsa",
+  //   img: "https://www.dicaspetz.com.br/wp-content/uploads/2019/01/como-cuidar-de-passarinho3.jpg",
+  //   rarity: "Common",
+  //   type: "Bird",
+  //   location: "New York",
+  // };
+
+  // await model.db.connect();
+  // const response = await model.postUser(user);
+  // console.log("response", response);
+  // await model.db.closeConnection();
+  await model.db.connect();
+  const a = await model.db.find({
+    $and: [
+      { name: "RZkXPX2yjd" },
+      { sighting: { $elemMatch: { name: "82mJlUgRWH" } } },
+    ],
+  });
+  console.log("a", a);
+
+  await model.db.closeConnection();
+};
+
+main();
