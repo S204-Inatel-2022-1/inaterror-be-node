a2e94698ad8cb66929815549bbb3626859b86040
bruno <bruno.3av@gmail.com>
2022-05-09 16:57:28 -0300

    added controllers

---
+++ src/controller/LoginController.ts
@@ -0,0 +1,32 @@
+import UserModel from "../model/UserModel";
+import { UserType } from "../types";
+
+export default class LoginController {
+  model = new UserModel();
+  public async login({ name, pass }: UserType) {
+    await this.model.db.connect();
+    const user = await this.model.getUserByLogin({
+      name: name,
+      pass: pass,
+    });
+    console.log("user", user);
+    await this.model.db.closeConnection();
+    return user;
+  }
+
+  public async register({ name, pass }: UserType) {
+    await this.model.db.connect();
+    const user = await this.model.postUser({ name, pass });
+    await this.model.db.closeConnection();
+    return user;
+  }
+}
+
+// async function View() {
+//   const lc = new LoginController();
+
+//   const user = await lc.login({ name: "test", pass: "test" });
+//   console.log("user", user);
+// }
+
+// View();
