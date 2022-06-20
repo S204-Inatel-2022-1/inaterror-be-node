6fdbf76bb005291c5d98d5a1ac1bed584aaf2544
bruno <bruno.3av@gmail.com>
2022-06-20 20:40:37 -0300

    Heroku

--- dist/src/server.js
+++ dist/src/server.js
@@ -11,5 +11,5 @@ const port = process.env.PORT || 3000;
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use(router_1.default);
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello World! 20h36 20/06'));
 app.listen(port, () => console.log(`listening on port ${port}! 😎 20h36 20/06`));
--- src/server.ts
+++ src/server.ts
@@ -7,5 +7,5 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello World! 20h36 20/06'));
 app.listen(port, () => console.log(`listening on port ${port}! 😎 20h36 20/06`));
